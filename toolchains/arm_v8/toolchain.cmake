acebc2fa4043d5855d4232552d9b996b8a4e72e9
samilarinc <msarinc@hotmail.com>
2025-09-30 16:28:02 +0300

    update submodule

--- toolchains/arm_v8/toolchain.cmake
+++ toolchains/arm_v8/toolchain.cmake
@@ -1,14 +1,10 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(TOOLCHAIN_TYPE arm_v8)
-set(CMAKE_CXX_COMPILER /opt/toolchains/gcc-linaro-10.2.1-2021.01-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-g++)
-set(CMAKE_C_COMPILER /opt/toolchains/gcc-linaro-10.2.1-2021.01-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-gcc)
-set(CMAKE_SYSTEM_PROCESSOR arm)
+set(CMAKE_CXX_COMPILER /opt/ulaq_share/arm_v8_2021_1/compiler/aarch64-linux/bin/aarch64-linux-gnu-g++)
+set(CMAKE_C_COMPILER /opt/ulaq_share/arm_v8_2021_1/compiler/aarch64-linux/bin/aarch64-linux-gnu-gcc)
+set(CMAKE_SYSTEM_PROCESSOR armv8l)
 
-set(CMAKE_SYSROOT /opt/toolchains/gcc-linaro-10.2.1-2021.01-x86_64_aarch64-linux-gnu/sysroot/libc)
+set(CMAKE_SYSROOT /opt/ulaq_share/arm_v8_2021_1/filesystem/rootfs)
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-
-set(CMAKE_SKIP_BUILD_RPATH FALSE)
-set(CMAKE_BUILD_WITH_INSTALL_RPATH TRUE)
-set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
